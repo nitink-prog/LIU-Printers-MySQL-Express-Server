fe61ac130ef34f0142f4b2e0f8633deb5019ce0f
nitink-prog <80429955+nitink-prog@users.noreply.github.com>
2022-04-21 10:28:43 -0400

    able to read PUT request body now

--- index.js
+++ index.js
@@ -20,8 +20,15 @@ db.connect((err) => {
 // ***** EXPRESS ***** //
 const app = express();
 const portExpress = "3008";
+app.use(express.json());
 
-// Get Printers DB - Pagination?
+app.get("/", (req, res) => {
+  res.send(
+    "Welcome to the Homepage of the LIU Printers MySQL Express Server. Built by Nitin K. Please reach an endpoint."
+  );
+});
+
+// Get Full Printers DB - Pagination?
 app.get("/printers", (req, res) => {
   let sql = "SELECT * FROM printers";
   let query = db.query(sql, (err, results) => {
@@ -33,14 +40,14 @@ app.get("/printers", (req, res) => {
 
 app
   .route("/printers/:id")
-  // Get Individual Printer
+  // Get a Printer
   .get((req, res) => {
     let id = req.params.id;
     let sql = `SELECT * FROM printers WHERE id = ${id}`;
-    let query = db.query(sql, (err, results) => {
+    let query = db.query(sql, (err, result) => {
       if (err) throw err;
-      console.log(results);
-      res.send(results);
+      console.log(result);
+      res.send(result);
     });
   })
   // Update a Printer
@@ -48,15 +55,17 @@ app
     let id = req.params.id;
     let sql = "UPDATE printers SET ?"; // UPDATE `printers` SET `dateUpdated` = NULL WHERE `printers`.`id` = 101;
     // TO-DO
+    res.json(req.body);
+    console.log(req.body);
   })
   // Insert a Printer
   .post((req, res) => {
     let id = req.params.id;
-    let printer = { 
+    let printer = {
       id,
       // TO-DO
     };
-    let sql = "INSERT INTO printers SET ?"; // INSERT INTO `printers` (`id`, `building`, `date_updated`, `department`, `mac_address`, `model`, `name`, `room`, `serial`) VALUES ('100', 'unknown', '2022-04-20', '', '60:12:8B:D5:0E:5B', '4235', 'B-HS-604-1', 'unknown', 'RKJ18475'); 
+    let sql = "INSERT INTO printers SET ?"; // INSERT INTO `printers` (`id`, `building`, `date_updated`, `department`, `mac_address`, `model`, `name`, `room`, `serial`) VALUES ('100', 'unknown', '2022-04-20', '', '60:12:8B:D5:0E:5B', '4235', 'B-HS-604-1', 'unknown', 'RKJ18475');
     let query = db.query(sql, printer, (err, result) => {
       if (err) throw err;
       console.log(result);
